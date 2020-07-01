d05b827b680ccea95cefc96d5e4803f548f934b8
今夕何夕兮 <zhaoyachao0414@163.com>
2020-07-01 15:57:42 +0800

    Update forms.py

--- my_blog/userprofile/forms.py
+++ my_blog/userprofile/forms.py
@@ -1,28 +1,60 @@
-#引入表单类
-from django import forms
-# 引入 User 模型
-from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from .forms import UserLoginForm,UserRegisterForm
 
-# 登录表单，继承了 forms.Form 类
-class UserLoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField()
+#用户注册
+def user_register(request):
+    if request.method == 'POST':
+        user_register_form = UserRegisterForm(data=request.POST)
+        if user_register_form.is_valid():
+            new_user = user_register_form.save(commit=False)
+            #设置密码
+            new_user.set_password(user_register_form.cleaned_data['password'])
+            new_user.save()
+            #保存好数据后立即登录并返回博客列表页面
+            login(request,new_user)
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("注册表单输入有误，请重新输入~")
+    elif request.method == 'GET':
+        user_register_form = UserRegisterForm()
+        context = {'form':user_register_form }
+        return render(request,'userprofile/register.html',context)
+    else:
+        return HttpResponse("请使用GET或POST请求数据")
 
-#注册用户表单
-class UserRegisterForm(forms.ModelForm):
-	#复写User的密码
-	password = forms.CharField()
-	password2 = forms.CharField()
 
-class Meta:
-	model = User
-	fields = ('username','email')
+# 引入logout模块
+from django.contrib.auth import authenticate, login, logout
 
-	#对两次的输入的密码是否一致进行检查
-	def clean_password2(self):
-		data = self.cleaned_data
-		if data.get('password') == data.get('password2'):
-			return data.get('password')
-		else:
-			raise forms.ValidationError("密码输入不一致，请重试");
+# Create your views here.
+#用户登录
+def user_login(request):
+    if request.method == 'POST':
+        user_login_form = UserLoginForm(data=request.POST)
+        if user_login_form.is_valid():
+            # .cleaned_data 清洗出合法数据
+            data = user_login_form.cleaned_data
+            # 检验账号、密码是否正确匹配数据库中的某个用户
+            # 如果均匹配则返回这个 user 对象
+            user = authenticate(username=data['username'], password=data['password'])
+            if user:
+                # 将用户数据保存在 session 中，即实现了登录动作
+                login(request, user)
+                return redirect("article:article_list")
+            else:
+                return HttpResponse("账号或密码输入有误。请重新输入~")
+        else:
+            return HttpResponse("账号或密码输入不合法")
+    elif request.method == 'GET':
+        user_login_form = UserLoginForm()
+        context = { 'form': user_login_form }
+        return render(request, 'userprofile/login.html', context)
+    else:
+        return HttpResponse("请使用GET或POST请求数据")
 
+# 用户退出
+def user_logout(request):
+    logout(request)
+    return redirect("article:article_list")
