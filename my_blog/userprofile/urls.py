8d6cdaa2b965d58d50211b093757b2401a8a3f34
今夕何夕兮 <zhaoyachao0414@163.com>
2020-07-01 16:04:12 +0800

    Update urls.py

--- my_blog/userprofile/urls.py
+++ my_blog/userprofile/urls.py
@@ -5,7 +5,8 @@ app_name = 'userprofile'
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
-    
     # 用户退出
     path('logout/', views.user_logout, name='logout'),
+    #用户注册
+    path('register/',views.user_register,name='register'),
 ]
