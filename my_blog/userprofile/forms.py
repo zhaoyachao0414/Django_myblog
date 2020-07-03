b22ac489a4e4372f132b42502c04140c9352aa0f
今夕何夕兮 <zhaoyachao0414@163.com>
2020-07-03 23:16:51 +0800

    Update forms.py

--- my_blog/userprofile/forms.py
+++ my_blog/userprofile/forms.py
@@ -2,22 +2,19 @@
 from django import forms
 # 引入 User 模型
 from django.contrib.auth.models import User
-
 # 登录表单，继承了 forms.Form 类
 class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField()
 
-#注册用户表单
+	#注册用户表单
 class UserRegisterForm(forms.ModelForm):
 	#复写User的密码
 	password = forms.CharField()
 	password2 = forms.CharField()
-
 class Meta:
 	model = User
 	fields = ('username','email')
-
 	#对两次的输入的密码是否一致进行检查
 	def clean_password2(self):
 		data = self.cleaned_data
